cb87571092371c72a0948df4e268dae027e16ccb
abd-zeyad <abd.d@live.com>
2022-02-08 23:24:36 +0200

    add to dbcontext

--- src/Boilerplate.Infrastructure/Context/ApplicationDbContext.cs
+++ src/Boilerplate.Infrastructure/Context/ApplicationDbContext.cs
@@ -11,12 +11,20 @@ namespace Boilerplate.Infrastructure.Context
         public DbSet<Hero> Heroes { get; set; }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Contractor> Contractors { get; set; }
+        public DbSet<Observation> Observations { get; set; }
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<SubSite> SubSites { get; set; }
+        public DbSet<ObservationPhoto> ObservationPhotos { get; set; }
+        public DbSet<ObservationType> ObservationTypes { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserConfiguration());
 
-                                       
+
         }
     }
 }
